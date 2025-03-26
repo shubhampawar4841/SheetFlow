d32d3dcfa999fbddd4999097b9671df9ab6adbdf
shubhampawar4841 <shubhampawar4036@gmail.com>
2025-03-26 23:46:49 +0530

    required

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -33,6 +33,8 @@ const Signup = () => {
         <AuthForm 
           isLogin={false} 
           onSubmit={handleSignup}
+          nameRequired
+          emailRequired
         />
       </div>
     </div>
